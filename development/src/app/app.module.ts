ed6a47b15b3850e801495c1744d75af1cef55552
Kyle Davis <kyle.davis@ucdenver.edu>
2023-04-05 15:07:34 -0600

    imported astroux

--- development/src/app/app.module.ts
+++ development/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AstroComponentsModule } from '@astrouxds/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
